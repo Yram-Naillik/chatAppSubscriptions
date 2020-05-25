02ed95d66632e36b858ae03c322c2228972eea0c
Killian Mary <killian.mary@etudiant.univ-lr.fr>
2020-05-25 11:14:25 +0200

    Adding first page of frontend

---
+++ server/frontend/src/App.vue
@@ -0,0 +1,65 @@
+
+<template>
+  <div id="app" class="container" style="padding-top: 100px">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-body">
+            <div class="row" v-if="entered">
+              <div class="col-md-12">
+                <div class="card">
+                  <div class="card-header">Chatbox</div>
+                  <div class="card-body">
+                    <dl v-for="(chat, id) in chats" :key="id">
+                      <dt>{{ chat.from }}</dt>
+                      <dd>{{ chat.message }}</dd>
+                    </dl>
+
+                    <hr />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row" v-else>
+              <div class="col-md-12">
+                <form method="post" @submit.prevent="enterChat">
+                  <div class="form-group">
+                    <div class="input-group">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Enter your username"
+                        v-model="username"
+                      />
+                      <div class="input-group-append">
+                        <button class="btn btn-primary" @click="enterChat">Enter</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      username: "",
+      message: "",
+      entered: false
+    };
+  },
+  methods: {
+    enterChat() {
+      this.entered = !!this.username != "";
+    }
+  }
+};
+</script>
